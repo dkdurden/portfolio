f84b6a57cf61d101b0778aa55871e74221b22d22
Dustin Durden <23153218+dkdurden@users.noreply.github.com>
2022-08-08 21:35:22 -0400

    Debugging Netlify build

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -1,6 +1,8 @@
 let PROJECT_ID = import.meta.env.SANITY_PROJECT_ID;
 let DATASET = import.meta.env.SANITY_DATASET;
 
+console.log(PROJECT_ID, DATASET);
+
 export async function query(q) {
   const encodedQuery = encodeURIComponent(q);
   const url = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${encodedQuery}`;
