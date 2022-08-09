801a7de97bea9a039c9ef58ea20b8b6af1c7f6e6
Dustin Durden <23153218+dkdurden@users.noreply.github.com>
2022-08-08 21:01:00 -0400

    Update social links

--- src/components/SocialLinks.svelte
+++ src/components/SocialLinks.svelte
@@ -3,7 +3,11 @@
 </script>
 
 <div>
-  <a href="/" class:inverted>
+  <a
+    href="https://github.com/dkdurden"
+    aria-label="user dkdurden on GitHub"
+    class:inverted
+  >
     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24">
       <path
         fill="white"
@@ -11,7 +15,11 @@
       />
     </svg>
   </a>
-  <a href="/" class:inverted>
+  <a
+    href="https://twitter.com/dkdurden"
+    aria-label="user dkdurden on Twitter"
+    class:inverted
+  >
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="20">
       <path
         fill="white"
@@ -19,13 +27,33 @@
       />
     </svg>
   </a>
-  <a href="/" class:inverted>
+  <a
+    href="https://www.linkedin.com/in/dustin-durden-292776125/"
+    aria-label="Dustin Durden on LinkedIn"
+    class:inverted
+  >
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
       <path
         fill="white"
         d="M21.6 0H2.4C1.08 0 0 1.08 0 2.4v19.2C0 22.92 1.08 24 2.4 24h19.2c1.32 0 2.4-1.08 2.4-2.4V2.4C24 1.08 22.92 0 21.6 0zM7.2 20.4H3.6V9.6h3.6v10.8zM5.4 7.56c-1.2 0-2.16-.96-2.16-2.16 0-1.2.96-2.16 2.16-2.16 1.2 0 2.16.96 2.16 2.16 0 1.2-.96 2.16-2.16 2.16zm15 12.84h-3.6v-6.36c0-.96-.84-1.8-1.8-1.8-.96 0-1.8.84-1.8 1.8v6.36H9.6V9.6h3.6v1.44c.6-.96 1.92-1.68 3-1.68 2.28 0 4.2 1.92 4.2 4.2v6.84z"
       />
     </svg>
+  </a>
+  <a
+    href="https://codepen.io/dkdurden"
+    aria-label="user dkdurden on CodePen"
+    class:inverted
+  >
+    <svg
+      width="24"
+      height="24"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      fill="white"
+      ><!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path
+        d="M502.285 159.704l-234-156c-7.987-4.915-16.511-4.96-24.571 0l-234 156C3.714 163.703 0 170.847 0 177.989v155.999c0 7.143 3.714 14.286 9.715 18.286l234 156.022c7.987 4.915 16.511 4.96 24.571 0l234-156.022c6-3.999 9.715-11.143 9.715-18.286V177.989c-.001-7.142-3.715-14.286-9.716-18.285zM278 63.131l172.286 114.858-76.857 51.429L278 165.703V63.131zm-44 0v102.572l-95.429 63.715-76.857-51.429L234 63.131zM44 219.132l55.143 36.857L44 292.846v-73.714zm190 229.715L61.714 333.989l76.857-51.429L234 346.275v102.572zm22-140.858l-77.715-52 77.715-52 77.715 52-77.715 52zm22 140.858V346.275l95.429-63.715 76.857 51.429L278 448.847zm190-156.001l-55.143-36.857L468 219.132v73.714z"
+      /></svg
+    >
   </a>
 </div>
 
