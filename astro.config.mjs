9e89d6f5e7013535c61221eff89a0c9a7e05a4d8
Dustin Durden <23153218+dkdurden@users.noreply.github.com>
2022-08-21 14:21:59 -0400

    Add astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable the Svelte renderer to support Svelte components.
-	renderers: ['@astrojs/renderer-svelte'],
+  // Enable the Svelte renderer to support Svelte components.
+  renderers: ["@astrojs/renderer-svelte"],
+  site: "https://dustindurden.com",
+  integrations: [sitemap()],
 });
